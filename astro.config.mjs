01cc7eef22bc80704582e5494f44ad3bcbea60cc
Nadun Deshitha <nadundeshitha828@gmail.com>
2025-02-27 00:49:02 +0530

    New Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nadundeshitha.github.io',
+	site: 'https://nadundeshitha.top',
 	integrations: [mdx(), sitemap()],
 });
